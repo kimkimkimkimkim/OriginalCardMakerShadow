0bdef9846f628a357455866f4ced997a46591a88
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-02-03 02:43:08 +0900

    ホーム画面の作成

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using GameBase;
+using UniRx;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    void Start()
+    {
+        HomeWindowFactory.Create(new HomeWindowRequest()).Subscribe();
+    }
+}
---
+++ Assets/Scripts/UI/Window/UIScript/HomeWindowUIScript.cs
@@ -0,0 +1,21 @@
+﻿using GameBase;
+
+[ResourcePath("UI/Window/Window-Home")]
+public class HomeWindowUIScript : WindowBase
+{
+    public override void Init(WindowInfo info)
+    {
+    }
+
+    public override void Open(WindowInfo info)
+    {
+    }
+
+    public override void Back(WindowInfo info)
+    {
+    }
+
+    public override void Close(WindowInfo info)
+    {
+    }
+}
