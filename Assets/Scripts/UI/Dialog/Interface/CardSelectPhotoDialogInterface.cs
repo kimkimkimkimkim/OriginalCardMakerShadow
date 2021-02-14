7f1053ddc4ac1f21fce731f9ae28cd612e0c5f10
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2021-02-14 16:17:01 +0900

    ダイアログUIの修正

---
+++ Assets/Scripts/UI/Dialog/Interface/CardSelectPhotoDialogInterface.cs
@@ -0,0 +1,18 @@
+﻿public class CardSelectPhotoDialogRequest
+{
+}
+
+public class CardSelectPhotoDialogResponse
+{
+    /// <summary>
+    /// 写真取得タイプ
+    /// </summary>
+    public PhotoType photoType { get; set; }
+}
+
+public enum PhotoType
+{
+    None,
+    Camera,
+    Library,
+}
